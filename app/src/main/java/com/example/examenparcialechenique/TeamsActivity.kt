f88b5058548c664ff024987670a98f5e0bb91576
Christopher Echenique <20100172@ue.edu.pe>
2024-05-18 10:16:20 -0500

    Layouts de 4 y 5 listos

--- app/src/main/java/com/example/examenparcialechenique/TeamsActivity.kt
+++ app/src/main/java/com/example/examenparcialechenique/TeamsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.examenparcialechenique
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,15 @@ class TeamsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnGo2Menu: Button = findViewById(R.id.btnGo2Menu)
+
+
+
+
+        btnGo2Menu.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
